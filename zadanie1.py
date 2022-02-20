a116085b2cd299512af7526513d81cd94ab06d95
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-02-20 18:35:17 +0300

    changed  file zadanie1.py

---
+++ zadanie1.py
@@ -0,0 +1,13 @@
+def concat(*args: str, Reversed=False) -> str:
+    stroka = ''
+    if not Reversed:
+        for iter in args:
+            stroka += iter
+    else:
+        for iter in reversed(args):
+            stroka += iter
+    return print(stroka)
+
+
+concat('Hello', ' ', 'world')
+concat('Hello', ' ', 'world', Reversed=True)
